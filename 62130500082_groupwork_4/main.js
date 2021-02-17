447e16c2ee146272884414606746e028dcc101f5
Worawit Makkasane <worawit.4516@mail.kmutt.ac.th>
2021-02-17 19:59:17 +0700

    [update]Version groupwork3

--- 62130500082_groupwork_4/main.js
+++ 62130500082_groupwork_4/main.js
@@ -1,105 +1,76 @@
-    const app = {
-        data() {
-            return {
-                msg: 'Hello & Welcome, Vue3',
-                show: { icon: true, searchbar: false },
-                inputTitle: '',
-                Box_image: false,
-                photos: [{
-                        src: 'images/1.jpg',
-                        title: 'Autumn',
-                        favor: false,
-                        like: 0,
-                        zoom: false
-                    },
-                    {
-                        src: 'images/2.jpg',
-                        title: 'Hug & Love',
-                        favor: false,
-                        like: 0,
-                        zoom: false
-                    },
-                    {
-                        src: 'images/3.jpg',
-                        title: 'Good Night',
-                        favor: false,
-                        like: 0,
-                        zoom: false
-                    },
-                    {
-                        src: 'images/4.jpg',
-                        title: 'Architecture',
-                        favor: false,
-                        like: 0,
-                        zoom: false
-
-                    },
-                    {
-                        src: 'images/5.jpg',
-                        title: 'HarryPotter',
-                        favor: false,
-                        like: 0,
-                        zoom: false
-                    }
-
-                ],
-
-
-            }
-        },
-
-        methods: {
-
-            toggle() {
-                this.show.icon = !this.show.icon
-                this.show.searchbar = !this.show.searchbar
-                this.inputTitle = ''
-
-            },
-
-            favorite(index) {
-                this.photos[index].favor = !this.photos[index].favor
-                this.photos[index].like = this.photos[index].like + 1
-            },
-
-            zoom(index) {
-
-                for (let i = 0; i < this.photos.length; i++) {
-                    if (i == index) {
-                        this.photos[i].zoom = true;
-                        console.log(index);
-                    } else {
-                        this.photos[i].zoom = false;
-                        console.log(i);
-                    }
-
+const app = {
+    data() {
+        return {
+            show: { icon: true, searchbar: false },
+            inputTitle: '',
+            Box_image: false,
+            pictures: [{
+                    src: "./image/1.jpg",
+                    text: "Death Standing",
+                    like: false,
+                    zoom: false
+                },
+                {
+                    src: "./image/2.jpg",
+                    text: "Blair Witch",
+                    like: false,
+                    zoom: false
+                },
+                {
+                    src: "./image/3.jpg",
+                    text: "Cyberpunk",
+                    like: false,
+                    zoom: false
                 }
-                this.Box_image = true
-            },
-            closeZoom() {
-                this.Box_image = false
-            }
+            ]
+        }
+    },
+
+    methods: {
+        toggle() {
+            this.show.icon = !this.show.icon
+            this.show.searchbar = !this.show.searchbar
+            this.inputTitle = ''
 
         },
-        computed: {
+        zoom(index) {
 
-            countpicture() {
-                return this.photos.length
-            },
+            for (let i = 0; i < this.pictures.length; i++) {
+                if (i == index) {
+                    this.pictures[i].zoom = true;
+                    console.log(index);
+                } else {
+                    this.pictures[i].zoom = false;
+                    console.log(i);
+                }
 
-            searchPhoto() {
-                return this.photos.filter(searching => {
-                    return searching.title.toLowerCase().includes(this.inputTitle.toLowerCase())
-                })
-
-            },
-            searchZoom() {
-                return this.photos.filter(t => t.zoom)
             }
+            this.Box_image = true
+        },
+        closeZoom() {
+            this.Box_image = false
+        },
+        toggleDone(index) {
+            this.pictures[index].like = !this.pictures[index].like
+        }
+    },
+    computed: {
+        countLikes() {
+            return this.pictures.filter(t => t.like).length
+        },
+        countpicture() {
+            return this.pictures.length
+        },
 
+        searchPhoto() {
+            return this.pictures.filter(searching => {
+                return searching.text.toLowerCase().includes(this.inputTitle.toLowerCase())
+            })
+
+        },
+        searchZoom() {
+            return this.pictures.filter(t => t.zoom)
         }
     }
-
-
-
-    mountedApp = Vue.createApp(app).mount('#app')
+}
+Vue.createApp(app).mount('#app')
